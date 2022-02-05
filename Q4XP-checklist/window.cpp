903e0c3546563b79a17a059ed34e5f86017e345d
EdwinWalela <edwinwalela@gmail.com>
2022-02-05 13:24:20 +0300

    plugin start

--- Q4XP-checklist/window.cpp
+++ Q4XP-checklist/window.cpp
@@ -3,6 +3,7 @@
 #include "XPLMGraphics.h"
 #include <string.h>
 #include <stdio.h>
+#include <st>
 #if IBM
 	#include <windows.h>
 #endif
@@ -17,3 +18,61 @@
 #ifndef XPLM300
 	#error This is made to be compiled against the XPLM300 SDK
 #endif
+
+
+static XPLMWindowID g_window;
+
+void draw(XPLMWindowID window_id, void * in_refcon);
+int handle_mouse(XPLMWindowID window_id, int x, int y, int is_down, void * in_refcon);
+void recieve_main_monitor_bounds(int monitorIndex, int leftBx, int topBx, int rightBx, int bottomBx, void * refcon);
+
+int dummy_mouse_handler(XPLMWindowID window_id, int x, int y, int is_down, void *in_refcon) { return xplm_CursorDefault; }
+XPLMCursorStatus dummy_cursor_status_handler(XPLMWindowID window_id, int x, int y, void * in_refcon) { return 0; }
+int dummy_wheel_handler(XPLMWindowID window_id, int x, int y, int wheel, int clicks, void * in_refcon) { return 0; }
+void dummy_key_handler(XPLMWindowID window_id, char key, XPLMKeyFlags flags,char virtual_key,void * in_refcon, int losing_focus){}
+
+
+PLUGIN_API int XPluginStart(
+	char * outName,
+	char * outSig,
+	char * outDesc)
+{
+	strcpy(outName, "Q4XPChecklistPlugin");
+	strcpy(outSig, "blizzard.checklist.q4xp");
+	strcpy(outDesc, "A checklist plugin for FlyJsim Q4XP");
+
+	// Left,Bottom,Right,Top
+	int desktop_bounds[4];
+
+	// Get desktop bounds
+	XPLMGetScreenBoundsGlobal(&desktop_bounds[0], &desktop_bounds[3], &desktop_bounds[2], &desktop_bounds[1]);
+
+	// Window Params
+	XPLMCreateWindow_t params;
+
+	params.structSize = sizeof(params);
+	params.left = desktop_bounds[0] + 100;
+	params.bottom = desktop_bounds[1] + 100;
+	params.right = desktop_bounds[0] + 400;
+	params.top = desktop_bounds[1] + 300;
+	params.visible = 1;
+	params.drawWindowFunc = draw;
+	params.handleMouseClickFunc = handle_mouse;
+	params.handleRightClickFunc = dummy_mouse_handler;
+	params.handleMouseWheelFunc = dummy_wheel_handler;
+	params.handleKeyFunc = dummy_key_handler;
+	params.handleCursorFunc = dummy_cursor_status_handler;
+	params.refcon = NULL;
+	params.layer = xplm_WindowLayerFloatingWindows;
+	params.decorateAsFloatingWindow = 1;
+
+	g_window = XPLMCreateWindowEx(&params);
+
+	XPLMSetWindowPositioningMode(g_window, xplm_WindowPositionFree, -1);
+	// keep size constant as window resives. Left and top edges in same place relative to the window
+	XPLMSetWindowGravity(g_window, 0, 1, 0, 1); 
+	XPLMSetWindowTitle(g_window, "Q4XP Checklist");
+
+	return (g_window != NULL);
+
+}
