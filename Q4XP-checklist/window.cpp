3fde9296a2bdf9aec44278aceda2f58635fa8beb
EdwinWalela <edwinwalela@gmail.com>
2022-02-05 12:27:42 +0300

    initial commit

---
+++ Q4XP-checklist/window.cpp
@@ -0,0 +1,19 @@
+
+#include "XPLMDisplay.h"
+#include "XPLMGraphics.h"
+#include <string.h>
+#include <stdio.h>
+#if IBM
+	#include <windows.h>
+#endif
+#if LIN
+	#include <GL/gl.h>
+#elif __GNUC__
+	#include <OpenGL/gl.h>
+#else
+	#include <GL/gl.h>
+#endif
+
+#ifndef XPLM300
+	#error This is made to be compiled against the XPLM300 SDK
+#endif
